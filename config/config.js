a7cb98dfae159373ac6d7ed68de8e0d516164095
Andrew Greer <62513150+andrewpaulgreer@users.noreply.github.com>
2020-08-17 11:16:29 -0400

    updated out commit

--- config/config.js
+++ config/config.js
@@ -1,11 +1,4 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyCqec5Mr6UbKsYnEL5To5-SDibzv7rx8AA",
-  authDomain: "geo-lure.firebaseapp.com",
-  databaseURL: "https://geo-lure.firebaseio.com",
-  projectId: "geo-lure",
-  storageBucket: "geo-lure.appspot.com",
-  messagingSenderId: "716055826577",
-  appId: "1:716055826577:web:31d025c769c610d7a3ef30",
-  measurementId: "G-SW5H9KV5VC"
+
   };
 
